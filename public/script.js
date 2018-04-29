732986e19edb96b9ee8f7ea5c338564c3e5a26c4
Andi Zhou <zhouandi423@gmail.com>
2018-04-29 19:47:57 -0400

    HTML: Add a "Add to cart" button with a "v-on:click" directive. JS: Add a method called "addItem" within the Vue object that adds on the "total".

--- public/script.js
+++ public/script.js
@@ -2,5 +2,14 @@ new Vue({
     el: '#app',  // the Vue will be attached in the #app dom.
     data: {
         total: 0  // will use a {{total}} somewhere in the html
+    },
+    methods: {
+        addItem: function() {
+            this.total += 9.99; // referring to the [total] of this Vue object
+        }
     }
 });
+
+// Directives: all starts with 'v-'
+// v-if -> a "if" statement
+// v-bind: -> binding a variable to a HTML attribute
