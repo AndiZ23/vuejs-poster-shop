9e09ab360802011a741b5bad8d111c9b25bde6d7
Andi Zhou <zhouandi423@gmail.com>
2018-04-29 18:23:17 -0400

    Defined the first Vue Object: 'total' in the shopping cart.

--- public/script.js
+++ public/script.js
@@ -1,1 +1,6 @@
-console.log('It works.');
+new Vue({
+    el: '#app',  // the Vue will be attached in the #app dom.
+    data: {
+        total: 0  // will use a {{total}} somewhere in the html
+    }
+});
