656afdb5799c98ab88311c396df8cfced2e63913
Andi Zhou <zhouandi423@gmail.com>
2018-04-29 20:57:54 -0400

    Use (value, key) to iterate through a list with "v-for"

--- public/script.js
+++ public/script.js
@@ -2,14 +2,16 @@ new Vue({
     el: '#app',  // the Vue will be attached in the #app dom.
     data: {
         total: 0,  // will use a {{total}} somewhere in the html
-        items: [
+        items: [    // a list for the data property
             { title: 'Item 1'},
             { title: 'Item 2'},
             { title: 'Item 3'}
-        ]
+        ],
+        cart: []
     },
     methods: {
-        addItem: function() {
+        addItem: function(index) {
+            console.log(index);
             this.total += 9.99; // referring to the [total] of this Vue object
         }
     }
