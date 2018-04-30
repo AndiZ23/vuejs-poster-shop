b422dab8549a2271b346faecbf468a697d93af73
Andi Zhou <zhouandi423@gmail.com>
2018-04-29 22:53:39 -0400

    Format the display of price by using a Vue Filter

--- public/script.js
+++ public/script.js
@@ -35,5 +35,10 @@ new Vue({
                 });
             }
         }
+    },
+    filters: {
+        currency: function(price){
+            return '$'.concat(price.toFixed(2));
+        }
     }
 });
